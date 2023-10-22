c095b3f41bfe50c5765e2ff79885b3f152ba1b97
Oleksandr <oleksandr.prudnikov@gigaclear.com>
2023-10-22 11:44:54 +0100

    int to str port

--- bot_app/config.py
+++ bot_app/config.py
@@ -25,5 +25,5 @@ MYSQL = {
     "user": os.getenv("MYSQL_USER"),
     "password": os.getenv("MYSQL_PASSWORD"),
     "db": os.getenv("MYSQL_DB_NAME"),
-    "port": os.getenv("MYSQL_PORT"),
+    "port": int(os.getenv("MYSQL_PORT")),
 }
--- local_extensions/db_methods/config.py
+++ local_extensions/db_methods/config.py
@@ -14,5 +14,5 @@ MYSQL = {
     "user": os.getenv("MYSQL_USER"),
     "password": os.getenv("MYSQL_PASSWORD"),
     "db": os.getenv("MYSQL_DB_NAME"),
-    "port": str(os.getenv("MYSQL_PORT")),
+    "port": int(os.getenv("MYSQL_PORT")),
 }
